b1feec58f3d3a8cd8b832ae36f85c9d736816be5
Lukasz Kowalski <kowalski@TAILOREDAPPS.PL>
2023-03-21 18:13:44 +0100

    docs

--- src/TailoredApps.Shared.DateTime/DateTimeProvider.cs
+++ src/TailoredApps.Shared.DateTime/DateTimeProvider.cs
@@ -1,17 +1,38 @@
 ﻿namespace TailoredApps.Shared.DateTime
 {
+    /// <summary>
+    /// Simple implementation used for <see cref="IDateTimeProvider"/> 
+    /// </summary>
     public class DateTimeProvider : IDateTimeProvider
     {
+        /// <summary>
+        /// Basic implementation wchich returns current system DateTime based on <see cref="System.DateTime.Now"/>
+        /// </summary>
         public System.DateTime Now => System.DateTime.Now;
 
+        /// <summary>
+        /// Basic implementation wchich returns current system DateTime converted to Utc based on <see cref="System.DateTime.UtcNow"/>
+        /// </summary>
         public System.DateTime UtcNow => System.DateTime.UtcNow;
 
+        /// <summary>
+        /// Basic implementation wchich returns current system Time of day based on <see cref="System.DateTime.TimeOfDay"/>
+        /// </summary>
         public System.TimeSpan TimeOfDay => System.DateTime.Now.TimeOfDay;
 
+        /// <summary>
+        /// Basic implementation wchich returns current system Time of day converted to Utc based on <see cref="System.DateTime.TimeOfDay"/>
+        /// </summary>
         public System.TimeSpan UtcTimeOfDaty => System.DateTime.UtcNow.TimeOfDay;
 
+        /// <summary>
+        /// Basic implementation wchich returns current system date based on <see cref="System.DateTime.Today"/>
+        /// </summary>
         public System.DateTime Today => System.DateTime.Today;
 
+        /// <summary>
+        /// Basic implementation wchich returns current system date converted to Utc based on <see cref="System.DateTime.Date"/>
+        /// </summary>
         public System.DateTime UtcToday => System.DateTime.UtcNow.Date;
     }
 }
--- src/TailoredApps.Shared.DateTime/IDateTimeProvider.cs
+++ src/TailoredApps.Shared.DateTime/IDateTimeProvider.cs
@@ -1,12 +1,33 @@
 ﻿namespace TailoredApps.Shared.DateTime
 {
+    /// <summary>
+    /// Simple interface for mocking <see cref="System.DateTime"/> used for unit testing and configuration in mocks time used in tests.
+    /// </summary>
     public interface IDateTimeProvider
     {
+        /// <summary>
+        /// Gets current system DateTime
+        /// </summary>
         System.DateTime Now { get; }
+        /// <summary>
+        /// Gets current system DateTime converted to Utc format.
+        /// </summary>
         System.DateTime UtcNow { get; }
+        /// <summary>
+        /// Gets Time of Day and returns Timespan
+        /// </summary>
         System.TimeSpan TimeOfDay { get; }
+        /// <summary>
+        /// Gets Time of Day and returns Timespan converted to Utc format
+        /// </summary>
         System.TimeSpan UtcTimeOfDaty { get; }
+        /// <summary>
+        /// Current system Date
+        /// </summary>
         System.DateTime Today { get; }
+        /// <summary>
+        /// Current system Date converted to Utc.
+        /// </summary>
         System.DateTime UtcToday { get; }
     }
 }
