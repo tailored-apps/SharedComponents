09824991c95de6844932eb5c4f2d99ee99d475ae
Lukasz Kowalski <kowalski@nieprzecietny.pl>
2023-12-06 18:02:36 +0100

    ML update

--- src/TailoredApps.Shared.MediatR.ImageClassification/Infrastructure/ImageClassificationOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TailoredApps.Shared.MediatR.ImageClassification.Infrastructure
-{
-    public class ImageClassificationOptions
-    {
-        public const string Position = "ImageClassification";
-        public const string ModelFilePathConfig = "ImageClassification:ModelFilePath";
-        public string ModelFilePath { get; set; }
-    }
-}
---
+++ tests/TailoredApps.Shared.MediatR.ML.Tests/MLEngineTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+using System.Threading.Tasks;
+using TailoredApps.Shared.MediatR.ImageClassification.Infrastructure;
+using TailoredApps.Shared.MediatR.ImageClassification.Interfaces.Infrastructure;
+using Xunit;
+
+namespace TailoredApps.Shared.Payments.Tests
+{
+    public class MLEngineTests
+    {
+
+        [Fact]
+        public async Task CanRegisterAndGetModel()
+        {
+            var host = Host.CreateDefaultBuilder()
+                 .ConfigureAppConfiguration(a => a.AddEnvironmentVariables())
+                 .ConfigureServices((_, services) =>
+                 {
+                     services.AddPredictionEngine(_.Configuration);
+                 }).Build();
+
+            var paymentService = host.Services.GetService<IClassificationService>();
+            var providers = paymentService.GetModelInfo();
+            Assert.NotNull(providers);
+        }
+
+
+    }
+}
