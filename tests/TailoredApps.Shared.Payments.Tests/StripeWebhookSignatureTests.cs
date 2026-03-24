4c1b0878258aa853b0edcf01c2bc07ec8b47031c
Szeregowy <szeregowy@madagascar.net.pl>
2026-03-24 14:08:15 +0100

    fix: add missing using System.Threading.Tasks + System.Collections.Generic to test files

--- tests/TailoredApps.Shared.Payments.Tests/StripeWebhookSignatureTests.cs
+++ tests/TailoredApps.Shared.Payments.Tests/StripeWebhookSignatureTests.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 using global::Stripe;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
